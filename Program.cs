38fade4522eb136b70c28965433b57e1e2d15132
Minerva_juppiter <94231606+minerva-jupiter@users.noreply.github.com>
2022-04-12 23:29:33 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool forEver = true;
+bool file;
+string fileName = @"CountDown.txt";
+Console.WriteLine("When is the target time?(yyyy/mm/dd hh:mm:ss)");
+string targetTimeString = Console.ReadLine();
+var targetTimeInt = DateTime.Parse(targetTimeString);
+
+file = File.Exists(fileName);
+Log(Convert.ToString(file));
+if (file == false)
+{
+    StreamWriter sw = File.CreateText(fileName);
+}
+while (forEver)
+{
+    StreamWriter sw = new StreamWriter(fileName);
+    var datNow = DateTime.Now;
+    var time = CountDown(targetTimeInt,datNow);
+    sw.WriteLine(time);
+    sw.Close();
+    Log(Convert.ToString(time));
+}
+
+
+TimeSpan CountDown(DateTime dtTarget, DateTime dtNow)
+{
+    var tsDiff = dtTarget - dtNow;
+    return tsDiff;
+}
+
+void Log(string message)
+{
+    Console.WriteLine(message);
+}
